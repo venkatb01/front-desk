f465ce7aeaff10a1b5db10d4d2be5f947d4847f7
Vidya-Telugu <vidyatelugu2023@gmail.com>
2025-08-06 18:08:00 +0530

    DB

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express=require("express");
 const cors=require("cors");
+const connectDB = require("./utils/db");
 
 const app=express();
 const PORT=process.env.PORT;
@@ -9,6 +10,8 @@ const PORT=process.env.PORT;
 app.use(cors({
     origin:["http://localhost:5173","https://front-desk-fx1y.onrender.com"]
 }));
+
+connectDB();
 
 
 app.listen(PORT,()=>{
---
+++ server/src/utils/db.js
@@ -0,0 +1,12 @@
+const mongoose=require("mongoose");
+
+async function connectDB(){
+    try{
+       await mongoose.connect(process.env.MONGO_DB_URI);
+       console.log("Database connected successfully")
+    }  catch(error){
+       console.log("Error occurred while connecting to database");
+    }  
+}
+
+module.exports=connectDB;
