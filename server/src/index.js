e9d275f2f6889ec7f94be1ceba9d9cc36db97e85
Vidya-Telugu <vidyatelugu2023@gmail.com>
2025-08-06 17:20:36 +0530

    IC

---
+++ server/src/index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express=require("express");
+const cors=require("cors");
+
+const app=express();
+const PORT=process.env.PORT;
+
+
+app.use(cors({
+    origin:["http://localhost:5173"]
+}));
+
+
+app.listen(PORT,()=>{
+    console.log(`Server started running at http://localhost:${PORT}`)
+});
